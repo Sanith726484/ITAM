71700315b3b70fbb0e9721fc6bd821573f3734dd
Sanith <sanith@navtech.io>
2024-07-04 12:21:00 +0530

    testing deploy

--- php/admin_login.php
+++ php/admin_login.php
@@ -46,10 +46,10 @@ function validateAdmin($username, $password) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login only</title>
+    <title>Admin Login</title>
 </head>
 <body>
-    <h1>Admin Login</h1>
+    <h1>Admin Login only</h1>
     
     <form action="" method="post">
         <label for="username">Username:</label>
